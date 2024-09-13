31205250d43cb6fed9c54d45fc50b2515db33ec0
k1ng alph0x <shashankintouch@gmail.com>
2024-09-13 18:39:18 +0530

    visited path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// const WEBSOCKET_URL = "ws://localhost:8000/ws";
-
-// function App() {
-//   var maze = [
-//     [1, 1, 0, 0, 0],
-//     [0, 1, 0, 1, 0],
-//     [0, 0, 0, 1, 0],
-//     [1, 1, 1, 1, 0],
-//     [0, 0, 0, 0, 0],
-//   ];
-//   var start_x = 0;
-//   var start_y = 0;
-
-//   var end_x;
-//   var end_y = 4;
-
-//   const [socket, setSocket] = useState(null);
-
-//   useEffect(() => {
-//     const websocket = new WebSocket(WEBSOCKET_URL);
-//     setSocket(websocket);
-//     return () => websocket.close();
-//   }, []);
-
-//   useEffect(() => {
-//     if (!socket) return;
-
-//     socket.onopen = () => {
-//       console.log("Socket connected");
-//     };
-//   }, [socket]);
-
-//   function sendMessage() {
-//     if (socket && socket.readyState === WebSocket.OPEN) {
-//       const data = { maze, start: [start_x, start_y], end: [end_x, end_y] };
-//       socket.send(JSON.stringify(data));
-//     } else {
-//       console.log("Socket not connected");
-//     }
-//   }
-
-//   return (
-//     <>
-//       <h1 className="text-teal-600">Websocket</h1>
-//       <button onClick={sendMessage}>Send data</button>
-//     </>
-//   );
-// }
-
-// export default App;
 import { useEffect, useState } from "react";
 
 const WEBSOCKET_URL = "ws://localhost:8000/ws";
@@ -70,6 +18,8 @@ function App() {
 
   const [socket, setSocket] = useState(null);
   const [maze, setMaze] = useState([]);
+
+  const [visited, setVisited] = useState([]);
 
   var start_x = 0;
   var start_y = 0;
@@ -99,17 +49,35 @@ function App() {
   useEffect(() => {
     const websocket = new WebSocket(WEBSOCKET_URL);
     setSocket(websocket);
+
+    websocket.onopen = () => {
+      console.log("Socket connected");
+    };
+
+    websocket.onmessage = (message) => {
+      //Todo - Wrap in try catch
+      const data = JSON.parse(message.data);
+      if (data.visited) {
+        setVisited(data.visited);
+      }
+      //else if (data.result != undefined) {
+      //   setVisited(data.visited);
+      // }
+      console.log(data);
+    };
+
     return () => websocket.close();
   }, []);
 
-  //Check the connection
-  useEffect(() => {
-    if (!socket) return;
+  // //Check the connection
+  // useEffect(() => {
+  //   if (!socket) return;
 
-    socket.onopen = () => {
-      console.log("Socket connected");
-    };
-  }, [socket]);
+  //   socket.onopen = () => {
+  //     console.log("Socket connected");
+  //   };
+
+  // }, [socket]);
 
   //Send data to the backend
   function sendMessage() {
@@ -128,21 +96,25 @@ function App() {
 
   return (
     <div className="text-center">
-      <h1 className="bg-green-500">MAZE SOLVER</h1>
-      <button
-        onClick={generateMaze}
-        className="rounded-lg bg-black text-white p-2"
-      >
-        Generate Maze
-      </button>
-      <button
-        onClick={sendMessage}
-        className="rounded-lg bg-black text-white p-2"
-      >
-        Generate Path
-      </button>
+      <h1 className="bg-green-400 text-slate-700 text-3xl p-3 rounded-sm ">
+        MAZE SOLVER
+      </h1>
+      <div className="bg-teal-300 m-2 p-2 space-x-5">
+        <button
+          onClick={generateMaze}
+          className="rounded-lg bg-black text-white p-2"
+        >
+          Generate Maze
+        </button>
+        <button
+          onClick={sendMessage}
+          className="rounded-lg bg-black text-white p-2"
+        >
+          Generate Path
+        </button>
+      </div>
       {maze.length > 0 && (
-        <div className="bg-red-500">
+        <div className="bg-green-200">
           {maze.map((row, index) => (
             <div key={index} className="flex justify-center p-2">
               <div className="flex">
@@ -156,6 +128,18 @@ function App() {
           ))}
         </div>
       )}
+
+      {/* Visited Nodes */}
+      <div>
+        <h2>visited path</h2>
+        <p>
+          {visited.map((data, index) => (
+            <span key={index}>
+              ({data[0]}, {data[1]})
+            </span>
+          ))}
+        </p>
+      </div>
     </div>
   );
 }
